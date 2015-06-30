c2e10a09a691c77469d17f00f377d4d157923356
giantoak <graham.mueller@giantoak.com>
2015-06-30 12:37:31 -0400

    ENH: ewma anomaly detection

--- spark/sparkburst.py
+++ spark/sparkburst.py
@@ -1,32 +1,3 @@
-#
-# Licensed to the Apache Software Foundation (ASF) under one or more
-# contributor license agreements.  See the NOTICE file distributed with
-# this work for additional information regarding copyright ownership.
-# The ASF licenses this file to You under the Apache License, Version 2.0
-# (the "License"); you may not use this file except in compliance with
-# the License.  You may obtain a copy of the License at
-#
-#    http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
-#
-
-"""
- Counts words in UTF8 encoded, '\n' delimited text received from the network every second.
- Usage: kafka_wordcount.py <zk> <topic>
-
- To run this on your local machine, you need to setup Kafka and create a producer first, see
- http://kafka.apache.org/documentation.html#quickstart
-
- and then run the example
-    `$ bin/spark-submit --jars external/kafka-assembly/target/scala-*/\
-      spark-streaming-kafka-assembly-*.jar examples/src/main/python/streaming/kafka_wordcount.py \
-      localhost:2181 test`
-"""
 from __future__ import print_function
 
 import sys
@@ -38,6 +9,10 @@ from kafka import SimpleProducer, KafkaClient
 from nltk.corpus import stopwords
 import pybursts
 from itertools import chain
+import pandas as pd, numpy as np
+ewma = pd.stats.moments.ewma
+import math
+
 
 
 def filt(a):
@@ -51,16 +26,34 @@ def send(x):
     for record in x:
         producer.send_messages("bursts",str(record))
 
+
+#process_ts
+def process_ts(data):
+    """Returns the timestamps in which a 3 standard deviation from the ewma occured"""
+    dates=np.array(map(lambda x: float(x), data[1])).astype('datetime64[s]')
+    ts=pd.DataFrame(dates,index=dates)
+    #bin by every 30 minutes 
+    bin_ts=ts.groupby(pd.TimeGrouper('30T')).size()
+    #create a 3 hour moving average
+    ma = ewma(bin_ts,span=6)
+    alpha = 2.0/(7.0)
+    mean = np.mean(ma)
+    factor = 3.0*np.std(ma)
+    ucl = mean+factor
+    lcl = mean-factor
+    return (bin_ts[ma>ucl])
+
+
 def returnText(x):
     return ' '.join([ y['caption']['text'].lower() for y in json.loads(x[1]) if y.get('caption') and y['caption'].get('text') and y['caption']['text'].strip() != ''])
     #return len(json.loads(x[1]))
 if __name__ == "__main__":
     if len(sys.argv) != 3:
-        print("Usage: kafka_wordcount.py <zk> <topic>", file=sys.stderr)
+        print("Usage: sparkburst.py <zk> <topic>", file=sys.stderr)
         exit(-1)
 
-    #kafka     = KafkaClient("10.3.2.75:9092")
-    #producer  = SimpleProducer(kafka)
+    kafka     = KafkaClient("10.3.2.75:9092")
+    producer  = SimpleProducer(kafka)
 
     sc = SparkContext(appName="PythonStreamingKafkaWordCount")
     ssc = StreamingContext(sc, 120)
@@ -68,8 +61,13 @@ if __name__ == "__main__":
     zkQuorum, topic = sys.argv[1:]
     kvs = KafkaUtils.createStream(ssc, zkQuorum, "spark-streaming-consumer", {topic: 1})
     #lines = 
-    lines=kvs.map(lambda x: json.loads(x[1])[0]).filter(lambda x: 'created_time' in x.keys()).map(lambda x: x['created_time'])
-    
+    lines=kvs.map(lambda x: json.loads(x[1])[0]).filter(lambda x: 'created_time' in x.keys())
+    #.map(lambda x: x['created_time'])
+    counts=kvs.map(lambda x: json.loads(x[1])[0]).filter(lambda x:'created_time' in x.keys()).map(lambda x: (("%.2f" % float(x['location']['latitude']),"%.2f" % float(x['location']['longitude'])),[str(x['created_time'])]))\
+    .reduceByKey(lambda x,y: x+y)#.map(lambda x: sorted(map(int,x[1])))
+#.map(lambda x:pybursts.kleinberg(x,s=2,gamma=0.5))
+    ma_all=counts.map(lambda x: (x[0],process_ts(x)))
+    ma_all.pprint()
     bursts=lines.map(lambda x: x.split('\n')).reduce(lambda x,y: sorted(map(int,x+y))).map(lambda x: pybursts.kleinberg(x,s=2,gamma=0.5))
    #reduceByWindow(lambda x,y: map(int,list(chain(*zip(x,y))))\
     #            ,lambda x,y: map(int,list(chain(*zip(x,y)))),240,120)
@@ -80,7 +78,7 @@ if __name__ == "__main__":
     #    .reduceByKey(lambda a, b: a+b).filter(filt)
     #producer.send_messages("instacounts","yo")
     #counts.pprint()
-    bursts.foreachRDD(lambda rdd: rdd.foreachPartition(send))
+    #bursts.foreachRDD(lambda rdd: rdd.foreachPartition(send))
     
 
     ssc.start()
